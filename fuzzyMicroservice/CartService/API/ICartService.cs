430a69a532ac55a705019e8d57d054d64740d776
US\cesrodriguez <cesrodriguez@deloitte.com>
2019-12-31 09:02:26 -0500

    Added CartService

---
+++ fuzzyMicroservice/CartService/API/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using DataCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartService.API
+{
+    public interface ICartService
+    {
+        List<CartDetails> SyncShoppingCart(string userEmail, List<Cart> cartUpdates);
+      //  List<CartDetails> GetShoopingCart(string userEmail);
+    }
+}
